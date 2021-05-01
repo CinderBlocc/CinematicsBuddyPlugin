0a74fa69c60426f892f91c47f37803c6563de139
CinderBlocc <cinder.block.rl@gmail.com>
2021-05-01 18:31:36 -0500

    0.9.9c

--- CinematicsBuddy/Source/SupportFiles/MacrosStructsEnums.h
+++ CinematicsBuddy/Source/SupportFiles/MacrosStructsEnums.h
@@ -6,7 +6,7 @@ extern std::shared_ptr<class CVarManagerWrapper> GlobalCvarManager;
 extern std::shared_ptr<class GameWrapper>        GlobalGameWrapper;
 
 // MACROS //
-#define PLUGIN_VERSION "0.9.9b"
+#define PLUGIN_VERSION "0.9.9c"
 
 //Generic defines
 #define EXTENSION_RECORDING ".txt"
